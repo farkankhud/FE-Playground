614eba2b3ece1e761a68e03b1957ac6b54635e62
sharique ali <sharique.ali2001@yahoo.com>
2026-08-10 05:38:33 +0000

    Delete custom messages commit

--- app/employeeaction/webapp/ext/controller/NewController.controller.js
+++ app/employeeaction/webapp/ext/controller/NewController.controller.js
@@ -7,22 +7,45 @@ sap.ui.define(['sap/ui/core/mvc/ControllerExtension',
 		// this section allows to extend lifecycle hooks or hooks provided by Fiori elements
 		override: {
 			/**
-             * Called when a controller is instantiated and its View controls (if available) are already created.
-             * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
-             * @memberOf playground.employeeaction.employeeaction.ext.controller.NewController
-             */
+			 * Called when a controller is instantiated and its View controls (if available) are already created.
+			 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
+			 * @memberOf playground.employeeaction.employeeaction.ext.controller.NewController
+			 */
 			onInit: function () {
 				// you can access the Fiori elements extensionAPI via this.base.getExtensionAPI
 				var oModel = this.base.getExtensionAPI().getModel();
+				this._bCreatingEmployee = false;
 			},
 			editFlow: {
+
+				onBeforeCreate: function () {
+					this._bCreatingEmployee = true;
+				},
 				onAfterSave: async function (oContext) {
-						debugger;
+					debugger;
+					var sEmployeeNo = oContext.context.getProperty("employeeNo");
+					if (this._bCreatingEmployee) {
+						// this._bCreatingEmployee = true;
+						MessageToast.show(
+							"Employee " + sEmployeeNo + " created successfully."
+						);
+						this._bCreatingEmployee = false;
+					}
+					else {
+						MessageToast.show(
+							"Employee " + sEmployeeNo + " updated successfully."
+						);
 
-                        var sEmployeeNo = oContext.context.getProperty("employeeNo");
+					}
 
+
+
+
+				},
+				onAfterDelete: async function (oContext) {
+                   
                         MessageToast.show(
-                            "Employee " + sEmployeeNo + " created successfully."
+                            "Employee deleted successfully."
                         );
                     }
 			}
